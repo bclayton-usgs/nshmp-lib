36e9e0b10ce68af2dce36406318a0d3482cd0a3c
Peter Powers <pmpowers@usgs.gov>
2019-05-30 07:15:38 -0600

    javadoc; cleanup; refactoring

--- src/gov/usgs/earthquake/nshmp/data/MutableXySequence.java
+++ src/gov/usgs/earthquake/nshmp/data/MutableXySequence.java
@@ -2,60 +2,79 @@ package gov.usgs.earthquake.nshmp.data;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Function;
+
+import com.google.common.primitives.Doubles;
 
 /**
  * Mutable variant of {@code XySequence}.
+ * 
+ * <p>Methods and classes in this package will always return a
+ * {@link MutableXySequence} if the instance is, in fact, mutable. Mutable
+ * sequences should not be considered thread safe.
+ * 
+ * <p>The mutator methods ({@link #set(int, double)}, {@link #add(double),
+ * etc...)} of this class return a reference to the {@code this} sequence for
+ * use inline.
  *
+ * <p>All data supplied to the factory methods in this interface is copied
+ * unless it is not necessary to do so. Users should consider {@link #copyOf()}
+ * where possible as x-values will never be replicated in memory.
+ * 
  * @author Peter Powers
  */
 public interface MutableXySequence extends XySequence {
 
   /**
    * Create a new sequence with mutable y-values from the supplied value arrays.
-   * If the supplied y-value array is null, all y-values are initialized to 0.
+   * If the supplied y-value array is null, y-values are initialized to 0.
    *
    * @param xs x-values to initialize sequence with
    * @param ys y-values to initialize sequence with; may be null
-   * @return a mutable, {@code double[]}-backed sequence
-   * @throws NullPointerException if {@code xs} are null
    * @throws IllegalArgumentException if {@code xs} and {@code ys} are not the
    *         same size
    * @throws IllegalArgumentException if {@code xs} does not increase
    *         monotonically or contains repeated values
    */
-  public static MutableXySequence create(double[] xs, double[] ys) {
-    return (MutableXySequence) Sequences.create(xs, ys, true);
+  static MutableXySequence create(double[] xs, double[] ys) {
+    return Sequences.constructMutable(
+        Arrays.copyOf(xs, xs.length),
+        (ys == null)
+            ? new double[xs.length]
+            : Arrays.copyOf(ys, ys.length));
   }
 
   /**
    * Create a new sequence with mutable y-values from the supplied value
-   * collections. If the y-value collection is null, all y-values are
+   * collections. If the supplied y-value collection is null, y-values are
    * initialized to 0.
    *
    * @param xs x-values to initialize sequence with
    * @param ys y-values to initialize sequence with; may be null
-   * @return a mutable, {@code double[]}-backed sequence
-   * @throws NullPointerException if {@code xs} are null
    * @throws IllegalArgumentException if {@code xs} and {@code ys} are not the
    *         same size
    * @throws IllegalArgumentException if {@code xs} does not increase
    *         monotonically or contains repeated values
    */
-  public static MutableXySequence create(
+  static MutableXySequence create(
       Collection<? extends Number> xs,
       Collection<? extends Number> ys) {
-    return (MutableXySequence) Sequences.create(xs, ys, true);
+
+    return Sequences.constructMutable(
+        Doubles.toArray(xs),
+        (ys == null)
+            ? new double[xs.size()]
+            : Doubles.toArray(ys));
   }
 
   /**
    * Create a mutable copy of the supplied {@code sequence}.
    *
    * @param sequence to copy
-   * @return a mutable copy of the supplied {@code sequence}
-   * @throws NullPointerException if the supplied {@code sequence} is null
    */
-  public static MutableXySequence copyOf(XySequence sequence) {
+  static MutableXySequence copyOf(XySequence sequence) {
     return new MutableArrayXySequence(checkNotNull(sequence), false);
   }
 
@@ -67,24 +86,89 @@ public interface MutableXySequence extends XySequence {
    * @return a mutable copy of the supplied {@code sequence}
    * @throws NullPointerException if the supplied {@code sequence} is null
    */
-  public static MutableXySequence emptyCopyOf(XySequence sequence) {
+  static MutableXySequence emptyCopyOf(XySequence sequence) {
     return new MutableArrayXySequence(checkNotNull(sequence), true);
   }
 
-  void set(int index, double value);
+  /**
+   * Sets the y-{@code value} at {@code index}.
+   * @param index of y-{@code value} to set.
+   * @param value to set
+   * @return {@code this} sequence, for use inline
+   * @throws IndexOutOfBoundsException if the index is out of range (
+   *         {@code index < 0 || index >= size()})
+   */
+  MutableXySequence set(int index, double value);
 
+  /**
+   * Add a {@code term} to the y-values of this sequence in place.
+   *
+   * @param term to add
+   * @return {@code this} sequence, for use inline
+   */
   MutableXySequence add(double term);
 
+  /**
+   * Add the supplied y-values to the y-values of this sequence in place.
+   *
+   * @param ys y-values to add
+   * @return {@code this} sequence, for use inline
+   */
   MutableXySequence add(double[] ys);
 
+  /**
+   * Add the y-values of a sequence to the y-values of this sequence in place.
+   *
+   * @param sequence to add
+   * @return {@code this} sequence, for use inline
+   * @throws IllegalArgumentException if
+   *         {@code sequence.xValues() != this.xValues()}
+   */
   MutableXySequence add(XySequence sequence);
 
+  /**
+   * Multiply ({@code scale}) the y-values of this sequence in place.
+   *
+   * @param scale factor
+   * @return {@code this} sequence, for use inline
+   */
   MutableXySequence multiply(double scale);
 
+  /**
+   * Multiply the y-values of this sequence by the y-values of another sequence
+   * in place.
+   *
+   * @param sequence to multiply {@code this} sequence by
+   * @return {@code this} sequence, for use inline
+   * @throws IllegalArgumentException if
+   *         {@code sequence.xValues() != this.xValues()}
+   */
   MutableXySequence multiply(XySequence sequence);
 
+  /**
+   * Set the y-values of this sequence to their complement in place [
+   * {@code 1 - y}]. Assumes this is a probability distribution limited to the
+   * domain [0..1].
+   *
+   * @return {@code this} sequence, for use inline
+   */
   MutableXySequence complement();
 
+  /**
+   * Sets all y-values to 0.
+   *
+   * @return {@code this} sequence, for use inline
+   */
   MutableXySequence clear();
+
+  /**
+   * Transforms all y-values in place using the supplied {@link Function}.
+   *
+   * @param function for transform
+   * @return {@code this} sequence, for use inline
+   */
+  default XySequence transform(Function<Double, Double> function) {
+    throw new UnsupportedOperationException();
+  }
 
 }
--- src/gov/usgs/earthquake/nshmp/data/XySequence.java
+++ src/gov/usgs/earthquake/nshmp/data/XySequence.java
@@ -1,26 +1,23 @@
 package gov.usgs.earthquake.nshmp.data;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.function.Function;
+import java.util.stream.DoubleStream;
+
+import com.google.common.primitives.Doubles;
 
 /**
- * A sequence of xy-value pairs that is iterable ascending in x. Once created,
- * the x-values of a sequence are immutable.
+ * An immutable sequence of xy-value pairs that is iterable ascending in x.
  * 
- * <p>Sequences returned by the factory methods of this interface are guaranteed
- * to also have immutable y-values. Methods and classes in this package will
- * always return the sub-type {@link MutableXySequence} if the instance is, in
- * fact, mutable. Mutable sequences should not be considered thread safe.
+ * <p>For mutable y-values, use the {@link MutableXySequence} sub-type. Methods
+ * and classes in this package will always return a {@link MutableXySequence} if
+ * the instance is, in fact, mutable. Mutable sequences should not be considered
+ * thread safe.
  * 
- * <p>All data supplied to the factory methods in this interface are defensively
- * copied unless it is not necessary to do so. For instance, {@code *copyOf()}
- * variants should be used where possible as x-values will never be replicated
- * in memory.
+ * <p>All data supplied to the factory methods in this interface is copied
+ * unless it is not necessary to do so. Users should consider {@link #copyOf()}
+ * where possible as x-values will never be replicated in memory.
  *
  * @author Peter Powers
  * @see MutableXySequence
@@ -28,129 +25,84 @@ import java.util.function.Function;
 public interface XySequence extends Iterable<XyPoint> {
 
   /**
-   * Create a new, immutable sequence from the supplied value arrays. Unlike
-   * {@link #create(double[], double[])}, the supplied y-value array may not be
-   * null.
+   * Create a new, immutable sequence from the supplied value arrays.
    *
    * @param xs x-values to initialize sequence with
    * @param ys y-values to initialize sequence with
-   * @return an immutable, {@code double[]}-backed sequence
-   * @throws NullPointerException if {@code xs} or {@code ys} are null
    * @throws IllegalArgumentException if {@code xs} and {@code ys} are not the
    *         same size
    * @throws IllegalArgumentException if {@code xs} does not increase
    *         monotonically or contains repeated values
    */
-  public static XySequence create(double[] xs, double[] ys) {
-    return Sequences.create(xs, checkNotNull(ys), false);
+  static XySequence create(
+      double[] xs,
+      double[] ys) {
+
+    return Sequences.construct(
+        Arrays.copyOf(xs, xs.length),
+        Arrays.copyOf(ys, ys.length));
   }
 
   /**
    * Create a new, immutable sequence from the supplied value collections.
-   * Unlike {@link #create(Collection, Collection)} , the supplied y-value
-   * collection may not be null.
    *
    * @param xs x-values to initialize sequence with
    * @param ys y-values to initialize sequence with
-   * @return an immutable, {@code double[]}-backed sequence
-   * @throws NullPointerException if {@code xs} or {@code ys} are null
    * @throws IllegalArgumentException if {@code xs} and {@code ys} are not the
    *         same size
    * @throws IllegalArgumentException if {@code xs} does not increase
    *         monotonically or contains repeated values
    */
-  public static XySequence create(
+  static XySequence create(
       Collection<? extends Number> xs,
       Collection<? extends Number> ys) {
-    return Sequences.create(xs, checkNotNull(ys), false);
+
+    return Sequences.construct(
+        Doubles.toArray(xs),
+        Doubles.toArray(ys));
   }
 
   /**
    * Create an immutable copy of the supplied {@code sequence}.
    *
    * @param sequence to copy
-   * @return an immutable copy of the supplied {@code sequence}
-   * @throws NullPointerException if the supplied {@code sequence} is null
    */
-  public static XySequence copyOf(XySequence sequence) {
-    return (sequence.getClass().equals(ArrayXySequence.class)) ? sequence
+  static XySequence copyOf(XySequence sequence) {
+    return (sequence.getClass().equals(ArrayXySequence.class))
+        ? sequence
         : new ArrayXySequence(sequence, false);
   }
 
   /**
-   * Create a resampled version of the supplied {@code sequence}. Method
-   * resamples via linear interpolation and does not extrapolate beyond the
-   * domain of the source {@code sequence}; y-values with x-values outside the
-   * domain of the source sequence are set to 0.
-   *
-   * @param sequence to resample
-   * @param xs resample values
-   * @return a resampled sequence
-   */
-  @Deprecated
-  public static XySequence resampleTo(XySequence sequence, double[] xs) {
-    // NOTE TODO this will support mfd combining
-    checkNotNull(sequence);
-    checkArgument(checkNotNull(xs).length > 0);
-
-    Interpolator interpolator = Interpolator.builder()
-        .build();
-
-    double[] yResample = interpolator.findY(sequence.xValues(), sequence.yValues(), xs);
-
-    // TODO disable extrapolation
-    if (true) {
-      throw new UnsupportedOperationException();
-    }
-    return MutableXySequence.create(xs, yResample);
-  }
-
-  /**
-   * Returns the x-value at {@code index}.
+   * Return the x-value at {@code index}.
    * @param index to retrieve
-   * @return the x-value at {@code index}
    * @throws IndexOutOfBoundsException if the index is out of range (
    *         {@code index < 0 || index >= size()})
    */
   double x(int index);
 
-  double xUnchecked(int index);
-
   /**
-   * Returns the y-value at {@code index}.
+   * Return the y-value at {@code index}.
    * @param index to retrieve
-   * @return the y-value at {@code index}
    * @throws IndexOutOfBoundsException if the index is out of range (
    *         {@code index < 0 || index >= size()})
    */
   double y(int index);
 
-  double yUnchecked(int index);
-
   /**
-   * Returns the number or points in this sequence.
-   * @return the sequence size
-   */
-  int size();
-
-  /**
-   * Returns an immutable {@code List} of the sequence x-values.
-   * @return the {@code List} of x-values
+   * Return an immutable {@code List} of the sequence x-values.
    */
   List<Double> xValues();
 
   /**
-   * Returns an immutable {@code List} of the sequence y-values.
-   * @return the {@code List} of y-values
+   * Return an immutable {@code List} of the sequence y-values.
    */
   List<Double> yValues();
 
   /**
-   * Returns an iterator over the {@link XyPoint}s in this sequence. For
-   * immutable implementations, the {@link XyPoint#set(double)} method of a
-   * returned point throws an {@code UnsupportedOperationException}.
+   * Return the number of points in this sequence.
    */
-  Iterator<XyPoint> iterator();
+  int size();
 
   /**
    * The first {@link XyPoint} in this sequence.
@@ -163,28 +115,18 @@ public interface XySequence extends Iterable<XyPoint> {
   XyPoint max();
 
   /**
-   * Returns {@code true} if all y-values are 0.0; {@code false} otherwise. a
+   * Returns {@code true} if all y-values are 0.0; {@code false} otherwise.
    */
   boolean isClear();
 
   /**
-   * Returns a new, immutable sequence that has had all leading and trailing
-   * zero-valued points ({@code y = 0}) removed. Any zero-valued points in the
-   * middle of this sequence are ignored.
+   * Returns a new sequence that has had all leading and trailing zero-valued
+   * points ({@code y = 0}) removed. Any zero-valued points in the middle of
+   * this sequence are ignored.
    * 
    * @throws IllegalStateException if {@link #isClear() this.isClear()} as empty
    *         sequences are not permitted
    */
   XySequence trim();
-
-  /**
-   * Transforms all y-values in place using the supplied {@link Function}.
-   *
-   * @param function for transform
-   * @return {@code this} sequence, for use inline
-   */
-  default XySequence transform(Function<Double, Double> function) {
-    throw new UnsupportedOperationException();
-  }
 
 }
